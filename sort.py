65910e648bae034602fe5504f28670f4044189c9
fadil703 <36188010+fadil703@users.noreply.github.com>
2019-11-20 14:21:11 +0700

    Add files via upload

---
+++ sort.py
@@ -0,0 +1,63 @@
+ 
+def selectionSort(nlist):
+	for i in range(len(nlist)):
+		minpos=i
+
+		for j in range(i+1, len(nlist)):
+			if nlist[minpos] > nlist[j]:
+				minpos = j
+
+		temp = nlist[i]
+		nlist[i] = nlist[minpos]
+		nlist[minpos] = temp
+
+def bubbleSort(nlist):
+    for passnum in range(len(nlist)-1,0,-1):
+        for i in range(passnum):
+            if nlist[i]>nlist[i+1]:
+                temp = nlist[i]
+                nlist[i] = nlist[i+1]
+                nlist[i+1] = temp
+
+def insertionSort(nlist):
+   for index in range(1,len(nlist)):
+
+     currentvalue = nlist[index]
+     position = index
+
+     while position>0 and nlist[position-1]>currentvalue:
+         nlist[position]=nlist[position-1]
+         position = position-1
+
+     nlist[position]=currentvalue
+
+def mergeSort(nlist):
+    print("Splitting ",nlist)
+    if len(nlist)>1:
+        mid = len(nlist)//2
+        lefthalf = nlist[:mid]
+        righthalf = nlist[mid:]
+
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
+        i=j=k=0       
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                nlist[k]=lefthalf[i]
+                i=i+1
+            else:
+                nlist[k]=righthalf[j]
+                j=j+1
+            k=k+1
+
+        while i < len(lefthalf):
+            nlist[k]=lefthalf[i]
+            i=i+1
+            k=k+1
+
+        while j < len(righthalf):
+            nlist[k]=righthalf[j]
+            j=j+1
+            k=k+1
+    print("Merging ",nlist)
+
